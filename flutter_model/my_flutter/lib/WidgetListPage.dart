01e82c6b6ccb50b949b80621f0544f5f0f9e9748
xueh <644738532@qq.com>
2019-12-03 17:57:33 +0800

    常用组件

--- flutter_model/my_flutter/lib/WidgetListPage.dart
+++ flutter_model/my_flutter/lib/WidgetListPage.dart
@@ -15,7 +15,6 @@ class _WidgetListState extends State<WidgetListPage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -31,10 +30,11 @@ class _WidgetListState extends State<WidgetListPage> {
             children: <Widget>[
               Text(
                 _count.toString(),
-                maxLines: 1,//最大行数1
+                maxLines: 1, //最大行数1
                 textAlign: TextAlign.center,
                 overflow: TextOverflow.ellipsis, //设置超过显示省略号
-                style: TextStyle(fontWeight: FontWeight.bold,color: Colors.amber),
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.amber),
               ),
               OutlineButton(
                   onPressed: () {},
@@ -67,10 +67,31 @@ class _WidgetListState extends State<WidgetListPage> {
                 width: 100.0,
                 height: 100.0,
               ),
+              Row(
+                // 创建一组水平的子组件集合
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Widget One',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  Text('Widget Two'),
+                ],
+              ),
+//              //设置 margin 或者 padding 的 Widget。
+              SizedBox(height: 20),
+              Container(
+                //1）给一些没有自带宽高的属性设置宽高。
+                //比如我们上面讲到 Row 最后一个 Sample 的时候，给 Text、Row 设置宽高都是在外面套一层 Container。
+                //2) 设置间距。
+                padding: EdgeInsets.only(top: 20.0),
+                color: Colors.blue,
+                child: Image.network(   "http://img3.duitang.com/uploads/item/201504/07/20150407H4809_fzN5t.thumb.700_0.jpeg",width: 100,height: 100,),
+              ),
             ],
           ),
         ),
-
         floatingActionButton: FloatingActionButton(
           onPressed: _incrementCount,
           tooltip: 'Increment', //长按之后会显示的提示文字。
